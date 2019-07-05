1b14d128cb0df88eb79deb37a9dbe82cded586ac
Wilde <gutopicciani@yahoo.com.br>
2019-07-05 01:23:18 -0300

    Começando mapa no tiled

---
+++ tiled/tileset-maze.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tileset-maze" tilewidth="32" tileheight="32" tilecount="48" columns="16">
+ <image source="tileset-maze.png" width="512" height="96"/>
+</tileset>
