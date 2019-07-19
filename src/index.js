d555658086e4799711de50f2349f8e9bdbd0d718
Wilde <gutopicciani@yahoo.com.br>
2019-07-19 00:09:09 -0300

    Copiando estrutura

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
-import './index.sass';
-import Phaser from 'phaser';
-import config from './config';
+import Phaser from "phaser";
+import logoImg from "./assets/logo.png";
 
-const boot = () => {
-  return new Phaser.Game(config);
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scale: {
+    mode: Phaser.DOM.FIT,
+    width: '100%',
+    height: '100%',
+  },
+  scene: {
+    preload: preload,
+    create: create,
+  },
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image("logo", logoImg);
 }
 
-boot();
+function create() {
+  const logo = this.add.image(300, 150, "logo");
+
+  this.tweens.add({
+    targets: logo,
+    y: 450,
+    duration: 2000,
+    ease: "Power2",
+    yoyo: true,
+    loop: -1,
+  });
+}
